e89689e56cb73ee4e47fcfef13028f70fd7a4ea9
cnrkddl <163301801+cnrkddl@users.noreply.github.com>
2024-03-18 23:02:02 +0900

    Title: 정수 세 개 입력받아 출력, Time: 175ms, Memory: 14MB, Status: Passed - CodeTree

--- 240318/정수 세 개 입력받아 출력/take-three-integers-and-output.js
+++ 240318/정수 세 개 입력받아 출력/take-three-integers-and-output.js
@@ -1,16 +1,13 @@
 const fs = require('fs');
 
 // 표준 입력을 문자열로 읽어들임
-let inputString = fs.readFileSync(0);
+let inputString = fs.readFileSync(0, 'utf8');
 
-// 문자열로 변환 후 공백을 기준으로 문자열을 분리하여 배열로 저장
-let inputArray = inputString.toString().split(' ');
-
-// 개행 문자를 기준으로 문자열을 분리하여 배열로 저장
-let inputLines = inputString.toString().split('\n');
+// 공백과 개행 문자를 기준으로 문자열을 분리하여 배열로 저장
+let inputArray = inputString.split(/[ \n]+/);
 
 let a = Number(inputArray[0]);
 let b = Number(inputArray[1]);
-let c = Number(inputLines[1]);
+let c = Number(inputArray[2]);
 
 console.log(a, b, c);
