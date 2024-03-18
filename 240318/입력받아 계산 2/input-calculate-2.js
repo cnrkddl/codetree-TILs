f80c7848f9132fd2f883d0e436efaf60ab32d4dc
cnrkddl <163301801+cnrkddl@users.noreply.github.com>
2024-03-18 22:27:07 +0900

    Title: 입력받아 계산 2, Time: 216ms, Memory: 14MB, Status: Passed - CodeTree

--- 240318/입력받아 계산 2/input-calculate-2.js
+++ 240318/입력받아 계산 2/input-calculate-2.js
@@ -6,3 +6,14 @@ let n = arr[0];
 let m = arr[1];
 
 console.log(n*m);
+
+
+/*변수 선언, 입력
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().split(" ");
+
+let a = Number(input[0]);
+let b = Number(input[1]);
+
+// 출력
+console.log(a * b); */
