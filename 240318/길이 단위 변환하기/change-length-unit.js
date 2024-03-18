d9151e3d1e099a2465ea48194c34b705fa8bacfe
cnrkddl <163301801+cnrkddl@users.noreply.github.com>
2024-03-18 13:22:22 +0900

    Title: 길이 단위 변환하기, Time: 174ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240318/길이 단위 변환하기/change-length-unit.js
@@ -0,0 +1,11 @@
+let a = 9.2;
+let b =1.3;
+
+let ft = 30.48;
+let mi = 160934;
+
+let acm = a*ft;
+let bcm = b*mi;
+
+console.log(`${a}ft = ${acm.toFixed(1)}cm`);
+console.log(`${b}mi = ${bcm.toFixed(1)}cm`);
