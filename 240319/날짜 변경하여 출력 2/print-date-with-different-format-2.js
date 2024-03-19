f2ac941a6b91e0818e0dd25e81d0527cb96214cf
cnrkddl <163301801+cnrkddl@users.noreply.github.com>
2024-03-19 13:28:07 +0900

    Title: 날짜 변경하여 출력 2, Time: 193ms, Memory: 47MB, Status: Passed - CodeTree

--- 240319/날짜 변경하여 출력 2/print-date-with-different-format-2.js
+++ 240319/날짜 변경하여 출력 2/print-date-with-different-format-2.js
@@ -1,4 +1,7 @@
 const fs = require("fs");
-let input = fs.readFileSync(0).toString().trim();
+let input = fs.readFileSync(0).toString().trim().split("-");
+let year = Number(input[2]);
+let month= Number(input[0]);
+let day = Number(input[1]);
 
-console.log(input.split("-"))
+console.log(year+"."+month+"."+day);
