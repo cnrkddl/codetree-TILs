de8faa430bd7fc3198cacf64611ca4ba4d15445b
cnrkddl <163301801+cnrkddl@users.noreply.github.com>
2024-03-20 16:18:47 +0900

    Title: 삼항연산자, Time: 203ms, Memory: 14MB, Status: Passed - CodeTree

--- 240320/삼항연산자/ternary-operator.js
+++ 240320/삼항연산자/ternary-operator.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim();
 
-let a = Number(input[0]);
+let a = Number(input);
 
-let c = (a = 100) ? "pass" : "failure";
+let c = (a === 100) ? "pass" : "failure";
 
 console.log(c);
