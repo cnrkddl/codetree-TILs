7ee824e7a7a23da211b0c6f8f476fad0447e62ed
cnrkddl <163301801+cnrkddl@users.noreply.github.com>
2024-03-26 11:40:49 +0900

    Title: 숫자의 조건 여부 2, Time: 233ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240326/숫자의 조건 여부 2/number's-condition-2.js
@@ -0,0 +1,11 @@
+// 변수 선언 및 입력
+const fs = require('fs');
+let input = Number(fs.readFileSync(0).toString().trim());
+
+if (input ===5 ) {
+    console.log('A');
+}
+
+if (input % 2 === 0) {
+    console.log('B')
+}
