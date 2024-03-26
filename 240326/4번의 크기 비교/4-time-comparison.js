985e681e3e6e6cabc388e2a5663ffb8b8751a4bc
cnrkddl <163301801+cnrkddl@users.noreply.github.com>
2024-03-26 16:18:07 +0900

    Title: 4번의 크기 비교, Time: 178ms, Memory: 14MB, Status: Passed - CodeTree

--- 240326/4번의 크기 비교/4-time-comparison.js
+++ 240326/4번의 크기 비교/4-time-comparison.js
@@ -1,6 +1,16 @@
+// 입력 및 변수 선언
 let fs = require("fs");
-let input = fs.readFileSync(0, "utf8").trim().split("\n");
-let a = parseInt(input[0]);
-let values = input[1].split(" ").map(Number);
+let input = fs.readFileSync(0).toString().trim().split("\n");
+let arr = input[1].split(" ")
 
-console.log(values.map(val => a > val ? 1 : 0).join("\n"));
+let a = Number(input[0]);
+let b = Number(arr[0]);
+let c = Number(arr[1]);
+let d = Number(arr[2]);
+let e = Number(arr[3]);
+
+// 출력 
+console.log(a > b ? 1 : 0);
+console.log(a > c ? 1 : 0);
+console.log(a > d ? 1 : 0);
+console.log(a > e ? 1 : 0);
