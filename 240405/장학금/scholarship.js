a814bf9bb786d30a10312c78e91f7debeb54032c
cnrkddl <163301801+cnrkddl@users.noreply.github.com>
2024-04-05 00:08:32 +0900

    Title: 장학금, Time: 181ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240405/장학금/scholarship.js
+++ 240405/장학금/scholarship.js
@@ -4,15 +4,6 @@ let a = fs.readFileSync(0).toString().trim().split(" ");
 let result1;
 let result2;
 
-if (95<=a[0] &&a<=100) {
-    result1= 100000;
-}
-else if(90<=a[0]){
-    result1=50000;
-}
-else{
-    result1=0;
-}
 
 if (a[0]>=90 && (95<=a[1] &&a<=100)) {
     result2= 100000;
@@ -23,4 +14,4 @@ else if(a[0]>=90 && 90<=a[1]){
 else{
      result2=0;
     }
-console.log(result1 + result2);
+console.log(result2);
